5fff59094d64354c68d3f1aa4953c73c37605416
Sergei Kovalenko <kovalenkosergey.98@yandex.ru>
2020-02-01 20:07:13 +0400

    Logic for countdown

--- components/Timer.jsx
+++ components/Timer.jsx
@@ -7,19 +7,64 @@ export default class Timer extends Component {
   
         this.state = {
             intervalId: 0,
-            minutes: 25,
-            seconds: 10
+            minutes: 0,
+            seconds: 10,
+            isActive: false
         };
     }
+
+    onStart() {
+        const intervalId = setInterval(() => {
+            const { minutes, seconds } = this.state;
+
+            if (!seconds && !minutes) {
+                this.setState({
+                    minutes: 0,
+                    seconds: 0,
+                    isActive: false
+                });
+                return;
+            }
+
+            if (!seconds) {
+                this.setState({
+                    minutes: minutes - 1,
+                    seconds: 59
+                })
+            } else {
+                this.setState({
+                    seconds: seconds - 1
+                });
+            }
+        }, 1000);
+  
+        this.setState({
+            intervalId,
+            isActive: true
+        });
+    }
+  
+    onReset() {
+        const { intervalId } = this.state;
+    
+        clearInterval(intervalId);
+        this.setState({
+            minutes: 25,
+            seconds: 0,
+            isActive: false
+        });
+    }
+
+    padString = val => val.toString().length > 1 ? val : `0${val}` 
   
     render() {
-        const { minutes, seconds } = this.state;
+        const { minutes, seconds, isActive } = this.state;
 
         return (
             <View style={{ flex: 1 }}>
                 <View style={styles.container}>
                     <Text style={styles.paragraph}>
-                        {minutes}:{seconds}
+                        {this.padString(minutes)}:{this.padString(seconds)}
                     </Text>
                     <Image
                         style={styles.logo}
@@ -31,11 +76,14 @@ export default class Timer extends Component {
                         title="Start"
                         color="#FF0000"
                         accessibilityLabel="Start countdown"
+                        disabled={isActive}
+                        onPress={() => this.onStart()}
                     />
                     <Button
                         title="Reset"
                         color="#FF0000"
                         accessibilityLabel="Reset timer"
+                        onPress={() => this.onReset()}
                     />
                 </View>
             </View>
